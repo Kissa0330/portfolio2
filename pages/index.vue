92d6e4f6c442c9c056e52c0e73f0fe169e05db84
Kissa0330 <raika4789@gmail.com>
2020-10-29 13:47:56 +0900

    my first commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style>
+</style>
+
